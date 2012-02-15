74ba3fe6e0b78faf0739ac9b464fa8f63704879d
Mark Gardner <mjgardner@cpan.org>
2012-02-15 11:37:06 -0500

    initial commit

---
+++ lib/Tree/Path/Class.pm
@@ -0,0 +1,20 @@
+package Tree::Path::Class;
+
+use utf8;
+use strict;
+use Modern::Perl;
+use English '-no_match_vars';
+
+# VERSION
+
+1;
+
+# ABSTRACT: Main module for Tree-Path-Class
+
+=head1 SYNOPSIS
+
+    use Tree::Path::Class;
+
+=head1 DESCRIPTION
+
+Describe your module here.
